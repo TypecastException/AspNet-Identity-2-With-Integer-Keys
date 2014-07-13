c35bbc169890fc40d144fad1ef86ccff856d703b
xivsolutions <xivsolutions@gmail.com>
2014-07-12 21:40:27 -0500

    Add new Identity Models

--- IdentitySample/Models/IdentityModels.cs
+++ IdentitySample/Models/IdentityModels.cs
@@ -3,38 +3,97 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using System;
 
 namespace IdentitySample.Models
 {
-    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class ApplicationUser : IdentityUser
+    public class ApplicationUserLogin : IdentityUserLogin<int> { }
+    public class ApplicationUserClaim : IdentityUserClaim<int> { }
+    public class ApplicationUserRole : IdentityUserRole<int> { }
+
+    public class ApplicationRole : IdentityRole<int, ApplicationUserRole>, IRole<int>
     {
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        public string Description { get; set; }
+
+        public ApplicationRole() : base() { }
+        public ApplicationRole(string name)
+            : this()
         {
-            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
-            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
-            // Add custom user claims here
+            this.Name = name;
+        }
+
+        public ApplicationRole(string name, string description)
+            : this(name)
+        {
+            this.Description = description;
+        }
+    }
+
+
+    public class ApplicationUser : IdentityUser<int, ApplicationUserLogin, ApplicationUserRole, ApplicationUserClaim>, IUser<int>
+    {
+        public async Task<ClaimsIdentity>
+            GenerateUserIdentityAsync(UserManager<ApplicationUser, int> manager)
+        {
+            var userIdentity = await manager
+                .CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             return userIdentity;
         }
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+
+    public class ApplicationDbContext
+        : IdentityDbContext<ApplicationUser, ApplicationRole, int,
+        ApplicationUserLogin, ApplicationUserRole, ApplicationUserClaim>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("DefaultConnection")
         {
         }
 
         static ApplicationDbContext()
         {
-            // Set the database intializer which is run once during application start
-            // This seeds the database with admin user credentials and admin role
             Database.SetInitializer<ApplicationDbContext>(new ApplicationDbInitializer());
         }
 
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+    }
+
+
+    public class ApplicationUserStore :
+    UserStore<ApplicationUser, ApplicationRole, int,
+    ApplicationUserLogin, ApplicationUserRole, ApplicationUserClaim>, IUserStore<ApplicationUser, int>, IDisposable
+    {
+        public ApplicationUserStore()
+            : this(new IdentityDbContext())
+        {
+            base.DisposeContext = true;
+        }
+
+        public ApplicationUserStore(DbContext context)
+            : base(context)
+        {
+        }
+    }
+
+
+    public class ApplicationRoleStore
+    : RoleStore<ApplicationRole, int, ApplicationUserRole>,
+    IQueryableRoleStore<ApplicationRole, int>,
+    IRoleStore<ApplicationRole, int>, IDisposable
+    {
+        public ApplicationRoleStore()
+            : base(new IdentityDbContext())
+        {
+            base.DisposeContext = true;
+        }
+
+        public ApplicationRoleStore(DbContext context)
+            : base(context)
+        {
         }
     }
 }
