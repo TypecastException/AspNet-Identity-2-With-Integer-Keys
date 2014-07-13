3c6429b71fe706fa6acf962484a6769a04bedfa6
xivsolutions <xivsolutions@gmail.com>
2014-07-12 23:21:06 -0500

    Change Id property from string to int

--- IdentitySample/Models/AdminViewModel.cs
+++ IdentitySample/Models/AdminViewModel.cs
@@ -6,7 +6,7 @@ namespace IdentitySample.Models
 {
     public class RoleViewModel
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
         [Required(AllowEmptyStrings = false)]
         [Display(Name = "RoleName")]
         public string Name { get; set; }
@@ -14,7 +14,7 @@ namespace IdentitySample.Models
 
     public class EditUserViewModel
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
 
         [Required(AllowEmptyStrings = false)]
         [Display(Name = "Email")]
